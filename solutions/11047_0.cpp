f36b330ab25631f3026b0b94ccde060599f172fd
Seungchan Lee <imchan0325@gmail.com>
2024-04-03 16:02:06 +0900

    Solved 11047

---
+++ solutions/11047_0.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    int a[10] = {0, };
+    cin >> n >> k;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    reverse(a, a + n);
+
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        cnt += k / a[i];
+        k %= a[i];
+    }
+    cout <<
+}

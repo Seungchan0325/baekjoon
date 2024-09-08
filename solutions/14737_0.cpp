5d0fcd8d774638dcef7aa5392a2c667075e01850
Seungchan Lee <imchan0325@gmail.com>
2024-09-09 00:27:20 +0900

    Solved 14737

--- solutions/14737_0.cpp
+++ solutions/14737_0.cpp
@@ -10,15 +10,12 @@ using namespace std;
 #define DOWN(r, c) (5*TO_IDX(r, c) + 4)
 
 const int MAXW = 55;
-const int SZ = 5 * MAXW * MAXW;
-const int di[] = {0, -1, 1, 0, 0};
-const int dj[] = {0, 0, 0, -1, 1};
+const int SZ = 10 * MAXW * MAXW;
 
-int H, W, indeg[SZ];
-bool visited[MAXW], is_able[SZ];
+int H, W;
+bool visited[SZ], rev_visited[SZ];
 char puzzle[MAXW][MAXW];
-vector<int> graph[SZ];
-bitset<MAXW*MAXW> taken[SZ], take[SZ];
+vector<int> graph[SZ], tmp[SZ], rev_graph[SZ];
 
 bool inStack[SZ];
 int pv, num[SZ], low[SZ], scc[SZ], t;
@@ -26,7 +23,6 @@ stack<int> s;
 
 void find_scc(int u)
 {
-    cout << u / 5 << "\n";
     num[u] = low[u] = ++pv;
     s.push(u);
     inStack[u] = true;
@@ -62,6 +58,14 @@ void dfs(int u)
     }
 }
 
+void rev_dfs(int u)
+{
+    rev_visited[u] = true;
+    for(auto v : rev_graph[u]) {
+        if(!rev_visited[v]) rev_dfs(v);
+    }
+}
+
 int main()
 {
     cin >> H >> W;
@@ -72,16 +76,16 @@ int main()
     }
 
     int ball = -1;
-    vector<vector<int>> stars;
+    vector<array<int, 2>> stars;
     vector<pair<int, int>> edges;
 
     for(int i = 1; i <= H; i++) {
         for(int j = 1; j <= W; j++) {
-            cout << TO_IDX(i, j) << " ";
+            // cout << TO_IDX(i, j) << " ";
             if(puzzle[i][j] == '#') continue;
             else if(puzzle[i][j] == 'O') ball = STOP(i, j);
             else if(puzzle[i][j] == '*') {
-                stars.push_back({STOP(i, j), LEFT(i, j), RIGHT(i, j), UP(i, j), DOWN(i, j)});
+                stars.push_back({LEFT(i, j), UP(i, j)});
             }
 
             edges.emplace_back(STOP(i, j), LEFT(i, j));
@@ -98,10 +102,10 @@ int main()
             if(puzzle[i-1][j] == '.' || puzzle[i-1][j] == '*' || puzzle[i-1][j] == 'O') edges.emplace_back(UP(i, j), UP(i-1, j));
             else edges.emplace_back(UP(i, j), STOP(i, j));
 
-            if(puzzle[i+1][j] == '.' || puzzle[i+1][j] == '*' || puzzle[i][j+1] == 'O') edges.emplace_back(DOWN(i, j), DOWN(i+1, j));
+            if(puzzle[i+1][j] == '.' || puzzle[i+1][j] == '*' || puzzle[i+1][j] == 'O') edges.emplace_back(DOWN(i, j), DOWN(i+1, j));
             else edges.emplace_back(DOWN(i, j), STOP(i, j));
         }
-        cout << "\n";
+        // cout << "\n";
     }
 
     for(auto [u, v] : edges) {
@@ -120,57 +124,85 @@ int main()
         }
     }
 
-    for(int i = 0; i < stars.size(); i++) {
-        for(auto& j : stars[i]) {
-            j = scc[j];
-            if(j) taken[j].set(i);
-        }
-    }
-
     for(auto [u, v] : edges) {
         u = scc[u];
         v = scc[v];
 
         if(u == v || u == 0 || v == 0) continue;
 
-        cout << u << " " << v << "\n";
+        // cout << u << " " << v << "\n";
 
         graph[u].push_back(v);
-        indeg[v]++;
+        rev_graph[v].push_back(u);
     }
+
+    // cout << "----------------\n";
+
+    tmp[2*scc[ball]+1].push_back(2*scc[ball]);
+
+    for(int i = 0; i < stars.size(); i++) {
+        int a = scc[stars[i][0]];
+        int b = scc[stars[i][1]];
+        if(!a && !b) goto NO;
+        if(!a || !b) {
+            if(a == 0) a = b;
+            tmp[2*a+1].push_back(2*a);
+            // cout << 2*a+1 << " " << 2*a << "\n";
+        } else {
+            tmp[2*a+1].push_back(2*b);
+            // cout << 2*a+1 << " " << 2*b << "\n";
+            tmp[2*b+1].push_back(2*a);
+            // cout << 2*b+1 << " " << 2*a << "\n";
+        }
+    }
+    // cout << "----------------\n";
 
     for(int i = 1; i <= t; i++) {
         memset(visited, 0, sizeof(visited));
         dfs(i);
-
-        take[i] |= taken[i];
-        for(int j = 0; j < stars.size(); j++) {
-            for(auto k : stars[j]) {
-                if(visited[k]) take[i].set(j);
+        memset(rev_visited, 0, sizeof(rev_visited));
+        rev_dfs(i);
+        for(int j = 1; j <= t; j++) {
+            if(visited[j] == false && rev_visited[j] == false) {
+                tmp[2*i].push_back(2*j+1);
+                tmp[2*j].push_back(2*i+1);
+                // cout << 2*i << " " << 2*j+1 << "\n";
+                // cout << 2*j << " " << 2*i+1 << "\n";
             }
         }
     }
 
-    queue<pair<int, bitset<MAXW*MAXW>>> q;
-    q.emplace(scc[ball], taken[scc[ball]]);
-
-    while(!q.empty()) {
-        auto [u, bit] = q.front(); q.pop();
-        if(bit.count() == stars.size()) goto YES;
-        for(auto v : graph[u]) {
-            if((take[v] | bit).count() == stars.size()) {
-                is_able[v] = true;
-            }
-            if(--indeg[v] == 0) {
-                if((take[v] | bit).count() == stars.size()) {
-                    q.emplace(v, taken[v] | bit);
-                }
-            }
-        }
+    for(int i = 1; i <= t; i++) {
+        graph[i].clear();
     }
 
-    cout << "NO";
-    return 0;
-YES:
+    for(int i = 2; i <= 2*t+1; i++) {
+        graph[i] = tmp[i];
+    }
+
+    {
+    int old_t = t;
+
+    memset(inStack, 0, sizeof(inStack));
+    pv = t = 0;
+    memset(num, 0, sizeof(num));
+    memset(low, 0, sizeof(low));
+    memset(scc, 0, sizeof(scc));
+    s = stack<int>();
+
+    for(int i = 2; i <= 2*old_t+1; i++) {
+        if(!num[i]) find_scc(i);
+    }
+
+    for(int i = 1; i <= old_t; i++) {
+        if(scc[2*i] == scc[2*i+1]) {
+            goto NO;
+        }
+    }
+    }
+
     cout << "YES";
+    return 0;
+NO:
+    cout << "NO";
 }

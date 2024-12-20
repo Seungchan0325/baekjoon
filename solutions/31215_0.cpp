48663e2e8fc2b7d89b13f03d7487c1d855d69da8
Seungchan Lee <imchan0325@gmail.com>
2024-12-20 22:56:58 +0900

    Solved 31215

---
+++ solutions/31215_0.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--) {
+        int n;
+        cin >> n;
+        if(n >= 3) cout << "3\n";
+        else cout << "1\n";
+    }
+}

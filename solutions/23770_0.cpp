7b10bd06f4d64e5c4eb588dd30324c4d53e42f52
Seungchan Lee <imchan0325@gmail.com>
2025-06-23 17:30:34 +0900

    Solved 23770

---
+++ solutions/23770_0.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+struct Point {
+    ll x, y;
+    Point(ll _x = 0, ll _y = 0) : x(_x), y(_y) {}
+    auto operator <=> (const Point& rhs) const {
+        return pair(x, y) <=> pair(rhs.x, rhs.y);
+    }
+};
+
+ll ccw(Point a, Point b, Point c)
+{
+    ll ret = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y);
+    if(ret < 0) return -1;
+    if(ret > 0) return 1;
+    return 0;
+}
+
+vector<Point> convexHull(vector<Point> p)
+{
+    int n = p.size();
+    sort(p.begin(), p.end());
+    vector<Point> up, dw;
+    for(int i = 0; i < n; i++) {
+        while(up.size() >= 2 && ccw(up.end()[-2], up.end()[-1], p[i]) >= 0) up.pop_back();
+        while(dw.size() >= 2 && ccw(dw.end()[-2], dw.end()[-1], p[i]) <= 0) dw.pop_back();
+        up.push_back(p[i]);
+        dw.push_back(p[i]);
+    }
+    for(int i = up.size()-2; i >= 1; i--) dw.push_back(up[i]);
+    return dw;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n; cin >> n;
+    vector<Point> v(n);
+    for(auto& [x, y] : v) cin >> x >> y;
+    v = convexHull(v);
+    n = v.size();
+
+    if(n == 1) {
+        cout << "4\n";
+    } else if(n == 2) {
+        ll dx = abs(v[0].x - v[1].x);
+        ll dy = abs(v[0].y - v[1].y);
+        cout << 2 * max(dx, dy) + (dx == dy) + 4;
+    } else {
+        ll ans = 4;
+        for(int i = 1; i <= n; i++) {
+            ll dx = abs(v[i%n].x - v[i-1].x);
+            ll dy = abs(v[i%n].y - v[i-1].y);
+            ans += max(dx, dy);
+        }
+        cout << ans;
+    }
+}

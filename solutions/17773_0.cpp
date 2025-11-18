2cb1c2d501c19853e1fe0b2a2b61b30cfbc7b8bc
Seungchan Lee <imchan0325@gmail.com>
2025-11-18 17:45:43 +0900

    Solved 17773

---
+++ solutions/17773_0.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+
+const int MAXK = 101010;
+
+struct Node {
+    ll on;
+    ll off;
+};
+
+ll M, N, K, A[MAXK], B[MAXK], C[MAXK], D[MAXK];
+Node tree[8*MAXK];
+int lazy[8*MAXK];
+
+Node merge(Node a, Node b)
+{
+    return {a.on + b.on, a.off + b.off};
+}
+
+void prop(int s, int e, int idx)
+{
+    if(lazy[idx]) {
+        swap(tree[idx].on, tree[idx].off);
+        if(s != e) {
+            lazy[2*idx] ^= 1;
+            lazy[2*idx+1] ^= 1;
+        }
+        lazy[idx] = 0;
+    }
+}
+
+void init(vector<ll>& rs, int s, int e, int idx)
+{
+    if(s == e) {
+        tree[idx].on = rs[s+1] - rs[s];
+        return;
+    }
+    int m = (s + e) /2;
+    init(rs, s, m, 2*idx);
+    init(rs, m+1, e, 2*idx+1);
+    tree[idx] = merge(tree[2*idx], tree[2*idx+1]);
+}
+
+void upd(int l, int r, int s, int e, int idx)
+{
+    prop(s, e, idx);
+    if(e < l || r < s) return ;
+
+    if(l <= s && e <= r) {
+        lazy[idx] ^=1;
+        prop(s, e, idx);
+        return;
+    }
+    int m = (s + e) / 2;
+    upd(l, r, s, m, 2*idx);
+    upd(l, r, m+1, e, 2*idx+1);
+    tree[idx] = merge(tree[2*idx], tree[2*idx+1]);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    cin >> M >> N >> K;
+    for(int i = 1; i <= K; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
+
+    vector<ll> rs = {1, M+1};
+    for(int i = 1; i <= K; i++) {
+        rs.push_back(A[i]);
+        rs.push_back(B[i]+1);
+    }
+    sort(rs.begin(), rs.end());
+    rs.erase(unique(rs.begin(), rs.end()), rs.end());
+
+    vector<tuple<ll, ll, ll>> u;
+    for(int i = 1; i <= K; i++) {
+        int x = lower_bound(rs.begin(), rs.end(), A[i]) - rs.begin();
+        int y = lower_bound(rs.begin(), rs.end(), B[i]+1) - rs.begin() - 1;
+        u.emplace_back(C[i], x, y);
+        u.emplace_back(D[i]+1, x, y);
+    }
+
+    init(rs, 0, ssize(rs)-2, 1);
+    sort(u.begin(), u.end());
+    ll ans = 0;
+    ll pt = 1;
+    ll pon = M;
+    for(auto [t, x, y] : u) {
+        ans += (t - pt) * pon;
+        upd(x, y, 0, ssize(rs)-2, 1);
+        pon = tree[1].on;
+        pt = t;
+    }
+    ans += (N+1-pt) * pon;
+
+    cout << ans << "\n";
+}
